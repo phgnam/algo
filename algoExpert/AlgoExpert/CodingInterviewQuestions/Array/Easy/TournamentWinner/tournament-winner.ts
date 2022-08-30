8ed8ba2f760a0ca47765e9c7eb4d5b28fd1e9b0d
Phuong Nam <nam.nguyenphuong@savvycomsoftware.com>
2022-08-30 18:04:46 +0700

    tournament winner

---
+++ algoExpert/AlgoExpert/CodingInterviewQuestions/Array/Easy/TournamentWinner/tournament-winner.ts
@@ -0,0 +1,17 @@
+export function tournamentWinner(competitions: string[][], results: number[]) {
+  const a: {[key: string]: number} = {};
+  let winner = '';
+  let max = 0;
+  for (let i = 0; i < competitions.length; i++) {
+    const curWinner = results[i] ? competitions[i][0] : competitions[i][1];
+    a[curWinner] ? a[curWinner]++ : a[curWinner] = 1;
+    a[curWinner] > max ? ((max = a[curWinner]) && (winner = curWinner)) : (max = max);
+  }
+  return winner;
+}
+
+console.log(tournamentWinner([
+  ["HTML", "C#"],
+  ["C#", "Python"],
+  ["Python", "HTML"],
+], [0, 0, 1]))
